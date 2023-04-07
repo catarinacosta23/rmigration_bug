9858c15042b2437b46c67af941e29d2ca355c3e8
catarinacosta23 <61191879+catarinacosta23@users.noreply.github.com>
2023-04-07 21:52:38 +0100

    fix: made changes to fix bug #33586

--- rmigrate/models.py
+++ rmigrate/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
-# Create your models here.
-
-class A(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
-class B(models.Model):
-    a = models.ForeignKey('A', on_delete=models.PROTECT)
-
 class User(AbstractUser):
     pass
+
+class A(models.Model):
+    name = models.CharField(max_length=100, default='campo')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE, related_name='a_set')
+
+
+class B(models.Model):
+    a = models.ForeignKey('A', on_delete=models.PROTECT, null=False)
